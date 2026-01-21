4cb62fd0c41040a60dba1dd9c48cf6875d071593
Yoshi4489 <palat.chet@gmail.com>
2026-01-22 01:39:21 +0700

    Add login screen and configure routing with Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:thai_safe/features/authetication/screens/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +16,27 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Scaffold(),
+      routes: {
+        '/': (context) => MyHomePage(title: 'Home'),
+        '/login': (context) => LoginScreen(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text('Welcome to Thai Safe!'),
+      ),
     );
   }
 }
