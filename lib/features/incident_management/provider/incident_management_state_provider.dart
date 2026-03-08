132a670ce75c0584d939bae41f2717fd87efe68c
Yoshi4489 <palat.chet@gmail.com>
2026-03-08 13:42:19 +0700

    Refactor IncidentManagementState to separate incident lists by status and update loadIncidentByStatus method for improved state management

--- lib/features/incident_management/provider/incident_management_state_provider.dart
+++ lib/features/incident_management/provider/incident_management_state_provider.dart
@@ -6,42 +6,92 @@ import 'package:thai_safe/features/incidents/data/incident_model.dart';
 class IncidentManagementState {
   final bool isLoading;
   final String error;
-  final List<IncidentModel> incidents;
+  final List<IncidentModel> pendingIncidents;
+  final List<IncidentModel> acknowledgeIncidents;
+  final List<IncidentModel> inProgressIncidents;
+  final List<IncidentModel> resolvedIncidents;
+  final List<IncidentModel> cancelledIncidents;
 
   IncidentManagementState({
     this.isLoading = false,
     this.error = '',
-    this.incidents = const [],
+    this.pendingIncidents = const [],
+    this.acknowledgeIncidents = const [],
+    this.inProgressIncidents = const [],
+    this.resolvedIncidents = const [],
+    this.cancelledIncidents = const [],
   });
 
   IncidentManagementState copyWith({
     bool? isLoading,
     String? error,
-    List<IncidentModel>? incidents
+    List<IncidentModel>? pendingIncidents,
+    List<IncidentModel>? acknowledgeIncidents,
+    List<IncidentModel>? inProgressIncidents,
+    List<IncidentModel>? resolvedIncidents,
+    List<IncidentModel>? cancelledIncidents,
   }) {
     return IncidentManagementState(
       isLoading: isLoading ?? this.isLoading,
-      error:  error ?? this.error,
-      incidents: incidents ?? this.incidents
+      error: error ?? this.error,
+      pendingIncidents: pendingIncidents ?? this.pendingIncidents,
+      acknowledgeIncidents: acknowledgeIncidents ?? this.acknowledgeIncidents,
+      inProgressIncidents: inProgressIncidents ?? this.inProgressIncidents,
+      resolvedIncidents: resolvedIncidents ?? this.resolvedIncidents,
+      cancelledIncidents: cancelledIncidents ?? this.cancelledIncidents,
     );
   }
 }
 
-class IncidentManagementController extends StateNotifier<IncidentManagementState> {
+class IncidentManagementController
+    extends StateNotifier<IncidentManagementState> {
   final IncidentManagementService _incidentManagementService;
 
-  IncidentManagementController(this._incidentManagementService): super(IncidentManagementState());
+  IncidentManagementController(this._incidentManagementService)
+    : super(IncidentManagementState());
 
-  Future<void> loadIncidentByStatus(String status) async{
-    state.copyWith(error: "", isLoading: false);
+  Future<void> loadIncidentByStatus(String status) async {
+    state = state.copyWith(isLoading: true, error: "");
+
     try {
-      
-      state.copyWith(isLoading: null);
+      final incidents = await _incidentManagementService
+          .getIncidentByStatus(status)
+          .first;
+
+      switch (status) {
+        case "Pending":
+          state = state.copyWith(isLoading: false, pendingIncidents: incidents);
+          break;
+        case "Acknowledged":
+          state = state.copyWith(
+            isLoading: false,
+            acknowledgeIncidents: incidents,
+          );
+          break;
+        case "In Progress":
+          state = state.copyWith(
+            isLoading: false,
+            inProgressIncidents: incidents,
+          );
+          break;
+        case "Resolved":
+          state = state.copyWith(
+            isLoading: false,
+            resolvedIncidents: incidents,
+          );
+          break;
+        case "Cancelled":
+          state = state.copyWith(
+            isLoading: false,
+            cancelledIncidents: incidents,
+          );
+          break;
+      }
     } catch (e) {
-      state.copyWith(error: e.toString(), isLoading: false);
+      state = state.copyWith(isLoading: false, error: e.toString());
     }
   }
-} 
+}
 
 final incidentManagementController = Provider<IncidentManagementService>((ref) {
   return IncidentManagementService();
